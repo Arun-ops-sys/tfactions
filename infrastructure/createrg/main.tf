4b0bd8184914c72ab53c7438efe1ae5b76b1ba16
Arun <Arun-ops-sys@gmail.com>
2024-11-03 15:01:07 +0530

    create a simple workflow to create azure resource group

--- infrastructure/createrg/main.tf
+++ infrastructure/createrg/main.tf
@@ -1,15 +1,16 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.8.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source = "hashicorp/azurerm"
+#       version = "3.8.0"
+#     }
+#   }
+# }
 
 provider "azurerm" {
   #resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  skip_provider_registration = true
 }
 
 
